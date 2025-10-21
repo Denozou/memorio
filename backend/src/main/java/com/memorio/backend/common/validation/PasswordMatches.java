608062007ad7a21417301b23e31ca4066ae9c204
Denozou <anton.kolenchuk@gmail.com>
2025-10-21 15:24:06 +0200

    feat: implement password matching validation using custom annotations

--- backend/src/main/java/com/memorio/backend/common/validation/PasswordMatches.java
+++ backend/src/main/java/com/memorio/backend/common/validation/PasswordMatches.java
@@ -1,4 +1,13 @@
 package com.memorio.backend.common.validation;
-
-public interface PasswordMatches {
+import jakarta.validation.Constraint;
+import jakarta.validation.Payload;
+import java.lang.annotation.*;
+@Target({ElementType.TYPE})
+@Retention(RetentionPolicy.RUNTIME)
+@Constraint(validatedBy = PasswordMatchesValidator.class)
+@Documented
+public @interface PasswordMatches {
+    String message() default "Password and confirm password do not match";
+    Class<?>[] groups()default {};
+    Class<? extends Payload>[] payload() default {};
 }
--- backend/src/main/java/com/memorio/backend/common/validation/PasswordMatchesValidator.java
+++ backend/src/main/java/com/memorio/backend/common/validation/PasswordMatchesValidator.java
@@ -1,4 +1,23 @@
 package com.memorio.backend.common.validation;
+import com.memorio.backend.auth.dto.RegisterRequest;
+import jakarta.validation.ConstraintValidator;
+import jakarta.validation.ConstraintValidatorContext;
 
-public class PasswordMatchesValidator {
+public class PasswordMatchesValidator implements ConstraintValidator<PasswordMatches, RegisterRequest>{
+    @Override
+    public void initialize(PasswordMatches constraintAnnotation){
+
+
+    }
+    @Override
+    public boolean isValid(RegisterRequest request,
+                           ConstraintValidatorContext context){
+        if(request == null) return true; //let @NotNull handle null checks
+
+        String password = request.getPassword();
+        String confirmPassword = request.getConfirmPassword();
+        if(password == null || confirmPassword == null) return true; //let @NotBlank handle null/empty checks
+
+        return password.equals(confirmPassword);
+    }
 }
