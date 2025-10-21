4dec155f7d69bcca77dc425ed16ab2bbfb0fc2d2
Denozou <anton.kolenchuk@gmail.com>
2025-10-21 12:40:37 +0200

    feat: added password validation interfaces for matching checks

---
+++ backend/src/main/java/com/memorio/backend/common/validation/PasswordMatches.java
@@ -0,0 +1,4 @@
+package com.memorio.backend.common.validation;
+
+public interface PasswordMatches {
+}
---
+++ backend/src/main/java/com/memorio/backend/common/validation/PasswordMatchesValidator.java
@@ -0,0 +1,4 @@
+package com.memorio.backend.common.validation;
+
+public class PasswordMatchesValidator {
+}
